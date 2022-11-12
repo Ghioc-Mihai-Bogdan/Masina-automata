0094b95ceae6d4c2b7cf69b0afeead4c39a423e0
Ghioc-Mihai-Bogdan <114933120+Ghioc-Mihai-Bogdan@users.noreply.github.com>
2022-11-12 13:52:19 +0200

    Update ListaTranzitii.java

--- AFD/src/ListaTranzitii.java
+++ AFD/src/ListaTranzitii.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class ListaTranzitii {
 	
@@ -7,27 +8,32 @@ public class ListaTranzitii {
 	void adaugaTranzitie (Tranzitie tr) {
 		this.lista.add(tr);
 	}
-	
 	Tranzitie gasesteTranzitie(String stare, char simbol) {
 		for (int i = 0;i<this.lista.size();i++) {
 			Tranzitie tr = this.lista.get(i);
 			if(tr.spuneStInceput().equals(stare) && tr.spuneSimbol() == simbol) {
 				return tr;
-			}
-			
+			}			
 		}
 		return null;
+	}
+	void afiseazaAlfabet() {
+		Set s = Collections.synchronizedSet(new HashSet());
+		for (int i = 0; i<this.lista.size();i++) {
+			Tranzitie tr = this.lista.get(i);
+			s.add(tr.spuneSimbol());
+		}
+		JOptionPane.showMessageDialog(null,"\u03B5="+s);				
 	}
 	
 	public String toString() {
 		String iesire = "";
 		for (int i = 0;i<this.lista.size();i++) {
 			Tranzitie tr = this.lista.get(i);
-			iesire+="\u03B4("+tr.spuneStInceput()+","+tr.spuneSimbol()+")="+"{"+tr.spuneStSfarsit()+"}\n";
-			
+			iesire+="\u03B4("+tr.spuneStInceput()+","+tr.spuneSimbol()+")="+"{"+tr.spuneStSfarsit()+"}\n";			
 		}
 		return iesire;
 	}
 	
-
+	
 }
